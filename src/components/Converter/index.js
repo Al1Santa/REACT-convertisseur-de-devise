ddafa179486e0df00b415de0968d56ed785df9af
alain <alain.santamaria57@gmail.com>
2023-04-25 14:36:26 +0200

    test de isOpen, pas de maj car pas de state

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -12,10 +12,19 @@ import './styles.scss';
 
 // == Composant
 function Converter() {
-  const isOpen = false;
+  // par défaut les devises ne sont pas affichées
+  let isOpen = false;
+
+  const handleClick = () => {
+    console.log(`open avant le click : ${isOpen}`);
+    isOpen = true;
+    console.log(`open après le click : ${isOpen}`);
+  };
+
   return (
     <div className="converter">
       <Header baseAmount={1} />
+      <button type="button" onClick={handleClick}>Afficher / Cacher les devises </button>
       {isOpen && <Currencies currencies={currenciesList} />}
       <Amount currency="United States Dollar" value={1.09} />
     </div>
