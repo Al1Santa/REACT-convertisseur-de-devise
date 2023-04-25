6a0037d2f63a4114b42b4e080efc9dfe938b29da
alain <alain.santamaria57@gmail.com>
2023-04-25 09:54:45 +0200

    mise en place du style

---
+++ src/components/Converter/index.js
@@ -0,0 +1,17 @@
+// == Import
+
+import './styles.scss';
+
+// == Composant
+function Converter() {
+  return (
+    <div className="converter">
+      <div>Header</div>
+      <div>Currencies</div>
+      <div>Amount</div>
+    </div>
+  );
+}
+
+// == Export
+export default Converter;
