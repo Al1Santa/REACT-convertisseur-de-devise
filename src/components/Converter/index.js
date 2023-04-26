524e9e0ac202144aa634c1644b4b9104baab9181
alain <alain.santamaria57@gmail.com>
2023-04-26 09:35:47 +0200

    ispoen, bind => this

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,4 +1,8 @@
-// == Import
+// == Import npm
+import React from 'react';
+
+// == Import local
+
 // == Data
 import currenciesList from 'src/data/currencies';
 // == Composant
@@ -11,25 +15,33 @@ import './styles.scss';
 // console.log(currenciesList);
 
 // == Composant
-function Converter() {
+class Converter extends React.Component {
   // par défaut les devises ne sont pas affichées
-  let isOpen = false;
+  // let isOpen = false;
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
 
-  const handleClick = () => {
-    console.log(`open avant le click : ${isOpen}`);
-    isOpen = true;
-    console.log(`open après le click : ${isOpen}`);
-  };
+  handleClick() {
+    this.setState({
+      isOpen: true,
+    });
+  }
 
-  return (
+  render() {
+    return (
     <div className="converter">
       <Header baseAmount={1} />
-      <button type="button" onClick={handleClick}>Afficher / Cacher les devises </button>
-      {isOpen && <Currencies currencies={currenciesList} />}
+      <button type="button" onClick={this.handleClick}>Afficher / Cacher les devises </button>
+      {this.state.isOpen && <Currencies currencies={currenciesList} />}
       <Amount currency="United States Dollar" value={1.09} />
     </div>
-  );
+    );
 }
-
+}
 // == Export
 export default Converter;
